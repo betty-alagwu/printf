68ded4ef6b4cbd84951e827404c20a0e7da2a2e3
Ugly_Betty🙈 <bettyalagwu@gmail.com>
2022-10-18 17:09:47 +0100

    fix wrte_integer description

--- _printf.c
+++ _printf.c
@@ -58,6 +58,9 @@ int _printf(const char *format, ...)
 int i = 0;
 int total_characters_printed = 0;
 va_list arguments;
+if (format == NULL) {
+    return (-1);
+}
 va_start(arguments, format);
 while (format[i] != '\0')
 {
