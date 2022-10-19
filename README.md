cc292b5e3eb734d9c7cccae698c7ab246201025b
Betty Alagwu <bettyalagwu@gmail.com>
2022-10-19 03:01:28 +0100

    setup foundation _printf.c function

---
+++ README.md
@@ -0,0 +1,19 @@
+# This project is about coming up with a printf function in C language
+
+* It covers our own version of printf named _printf
+* This is an assignment from ALX school.
+<details>
+	<summary> 0. Write a function that produces output according to a format. </summary>
+	<ul>
+	<li> c </li>
+	<li> s </li>
+	<li> % </li>
+	</ul>
+</details>
+<details>
+	<summary> 1. Handle the following conversion specifiers: </summary>
+	<ul>
+	<li> d </li>
+	<li> i </li>
+	</ul>
+</details>
---
+++ _handle_formatted_print.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * _print_fmt_output - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: ind.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: 1 or 2;
+ */
+int _print_fmt_output(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, unknow_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = {
+		{'c', _print_single_char}, {'s', _handle_print_string}, {'%', _handle_print_pct},
+		{'i', _print_int}, {'d', _print_int}, {'b', _print_bin},
+		{'u', _handle_print_unsigned}, {'o', _handle_print_octal}, {'x', _print_hexdec},
+		{'X', _print_hexdec_up}, {'p', _handle_print_pointer}, {'S', _handle_non_printable},
+		{'r', _print_rev}, {'R', print_rot13string}, {'\0', NULL}
+	};
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+
+	if (fmt_types[i].fmt == '\0')
+	{
+		if (fmt[*ind] == '\0')
+			return (-1);
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+				--(*ind);
+			if (fmt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
+	}
+	return (printed_chars);
+}
